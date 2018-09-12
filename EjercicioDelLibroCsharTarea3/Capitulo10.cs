ac85c5890654d9e99be62485db9b8f7c6aca0be1
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-12 06:43:06 +0200

    Capitulo 10

---
+++ EjercicioDelLibroCsharTarea3/Capitulo10.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDelLibroCsharTarea3
+{
+    class Capitulo10
+    {
+     private int ID { get; set; }
+     private string Nombre { get; set }
+        
+    }
+}
