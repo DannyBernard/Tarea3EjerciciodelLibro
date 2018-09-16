21b480806ac114646edcfe38f3f38c65718a437c
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-16 07:01:39 +0200

    Terminado

--- EjercicioDelLibroCsharTarea3/Program.cs
+++ EjercicioDelLibroCsharTarea3/Program.cs
@@ -8,8 +8,26 @@ namespace EjercicioDelLibroCsharTarea3
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            Capitulo9 capitulo = new Capitulo9();
+            int op;
+            Console.WriteLine("Menu \n" + "1.Ejercicio 9.1\n" + "2.Ejercicio 9.3\n" + "3.Ejercicio 9.4\n" + "Elija una opcion: ");
+            op = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case 1:
+                    capitulo.Ejercicio1();
+                    break;
+                case 2:
+                    capitulo.Ejercicio3();
+                    break;
+                case 3:
+                    capitulo.Ejercicio4();
+                    break;
+                
+            }
         }
     }
 }
